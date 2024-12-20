082893fb491cee568b37c71ee96bc6065e84f9c8
Sarumi Atinuke Muslimah <81876671+Muslimahhh@users.noreply.github.com>
2024-12-20 23:33:40 +0100

    Update 102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,1 +1,12 @@
-#!/usr/bin/node
+const fs = require("fs");
+
+const files = process.argv.slice(2);
+
+files.forEach((file) => {
+  try {
+    const content = fs.readFileSync(file, "utf8");
+    console.log(content);
+  } catch (err) {
+    console.error(err);
+  }
+});
